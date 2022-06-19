c4059a0013ff5050b8cdc4cb2ed77003f22bb71a
Eric Thomas <eric@ericthomas.ca>
2022-06-19 01:09:08 -0230

    Experience

---
+++ src/main/java/ca/ericthomas/Experience.java
@@ -0,0 +1,89 @@
+package ca.ericthomas;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+/**
+ * Simple class to represent a student's experience. This class
+ * represents a single work experience. Students may have many
+ * of these.
+ *
+ * Experience has the following attributes:
+ * - ID - primary key
+ * - Title - job title
+ * - Employer - name of employer
+ * - Location - location of employer
+ * - Start Date - when the program started
+ * - End Date - when the program ended
+ *
+ * @author Eric Thomas
+ */
+@Entity(name = "education")
+public class Experience {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String employer;
+    private String location;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(String employer) {
+        this.employer = employer;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
