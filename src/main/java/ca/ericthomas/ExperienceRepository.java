cb69b9b51b70efd27b7932b8c064b111e184b6f9
Eric Thomas <eric@ericthomas.ca>
2022-06-19 01:02:39 -0230

    ExperienceRepository.java

---
+++ src/main/java/ca/ericthomas/ExperienceRepository.java
@@ -0,0 +1,10 @@
+package ca.ericthomas;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExperienceRepository extends CrudRepository <Experience, Integer> {
+
+}
+
